7161ead6cf0e079fdf7c92bf449ad12f812eec9b
saksh2410 <saksh1765@gmail.com>
2026-01-14 16:36:25 +0530

    setup poject config file

---
+++ src/config.py
@@ -0,0 +1,19 @@
+"""
+Configuration Module for the project.
+This module stores the global parameters such as data paths, feature selection configurations, model hyperparameters used throughout the project.
+"""
+
+# imports
+from pathlib import Path
+
+
+# raw data paths
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+RAW_DATA_PATH = PROJECT_ROOT / "data" / "raw"
+MOVIES = RAW_DATA_PATH / "tmdb_5000_movies.csv"
+CREDITS = RAW_DATA_PATH / "tmdb_5000_credits.csv"
+
+# Feature selection attributes
+KEEP_COLS = ['title','genres','keywords','overview', 'tagline', 'cast', 'crew']
+# Reformatting attributes
+CLEANUP_COLS = ['genres', 'keywords', 'cast']
